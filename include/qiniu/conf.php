5e354ac4e19d20c0cf29c467a75630d3fca2c64e
emu <emu@vip.qq.com>
2015-12-12 02:59:58 +0800

    init

---
+++ include/qiniu/conf.php
@@ -0,0 +1,22 @@
+<?php
+global $SDK_VER;
+
+global $QINIU_UP_HOST;
+global $QINIU_RS_HOST;
+global $QINIU_RSF_HOST;
+global $QINIU_API_HOST;
+global $QINIU_IOVIP_HOST;
+
+global $QINIU_ACCESS_KEY;
+global $QINIU_SECRET_KEY;
+
+$SDK_VER = "6.1.13";
+
+$QINIU_UP_HOST	= 'http://upload.qiniu.com';
+$QINIU_RS_HOST	= 'http://rs.qbox.me';
+$QINIU_RSF_HOST	= 'http://rsf.qbox.me';
+$QINIU_API_HOST = 'http://api.qiniu.com';
+$QINIU_IOVIP_HOST = 'http://iovip.qbox.me';
+
+$QINIU_ACCESS_KEY	= 'zTR9h8EOCDRQ3MiXecK8b4QUtTYHzzn7nE3icUOM';
+$QINIU_SECRET_KEY	= '-10LuumSdaJnkRImfE7WQiev-4QC6NFmnot41W1b';
